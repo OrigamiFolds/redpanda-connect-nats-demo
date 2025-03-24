6aa3a29ae3c8eb9b68528b06edb3d2d56e76b8fa
Mduduzi Sibisi <108700682+OrigamiFolds@users.noreply.github.com>
2025-03-24 10:14:05 +0200

    Added Project Files

---
+++ sensor-dispatcher/src/main/java/com/mycompany/sensor/dispatcher/SensorDispatcher.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.sensor.dispatcher;
+
+import io.nats.client.Connection;
+import io.nats.client.Nats;
+import io.nats.client.Dispatcher;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+/**
+ *
+ * @author beart
+ */
+public class SensorDispatcher {
+
+    public static void main(String[] args) {
+        String natsURL = "nats://127.0.0.1:4222";
+
+        try (Connection nc = Nats.connect(natsURL)) {
+
+            Dispatcher dispatcher = nc.createDispatcher((msg) -> {
+                System.out.printf("%s on subject %s\n",
+                        new String(msg.getData(), StandardCharsets.UTF_8),
+                        msg.getSubject());
+            });
+
+            dispatcher.subscribe("inventory");
+
+            Thread.sleep(20000);
+        } catch (InterruptedException | IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ sensor-publisher/src/main/java/draft/dev/sensor/publisher/SensorPublisher.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package draft.dev.sensor.publisher;
+
+/**
+ *
+ * @author Mduduzi Sibisi
+ */
+import io.nats.client.Connection;
+import io.nats.client.Nats;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+        
+public class SensorPublisher {
+    private final static int ITEM_REMOVED = 0;
+    private final static int ITEM_ADDED = 1;
+    private final static int INVENTORY_LOW = 2;
+            
+    
+    public static void main(String[] args) {
+        String natsURL = "nats://127.0.0.1:4222";
+        
+        /*
+         * Add code to check that args[0] isn't null and is indeed an integer
+         * before parsing.
+         */
+        
+        int option = Integer.parseInt(args[0]);
+        
+        // Create timestamp and format it to match RedPanda Connect configuration and QuestDB
+        LocalDateTime currentTime = LocalDateTime.now();        
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(
+                "yyyy-MM-dd'T'HH:mm:ss.SSSSSS");
+        String formattedDateTime = currentTime.format(formatter);
+        
+        try (Connection nc = Nats.connect(natsURL)) {
+            String message = "";
+            // Switch expressions to check what type of message to send
+            switch (option) {
+                case ITEM_REMOVED -> message = 
+                        "{\"id\":\"0\",\"message\":\"Inventory item removed at:\",\"timestamp\":\"" + formattedDateTime + "\"}";
+                case ITEM_ADDED -> message = ""
+                        + "{\"id\":\"1\",\"message\":\"Inventory item added at:\",\"timestamp\":\"" + formattedDateTime + "\"}";
+                case INVENTORY_LOW -> message = 
+                        "{\"id\":\"2\",\"message\":\"The was low at:\",\"timestamp\":\"" + formattedDateTime + "\"}";
+            }
+            
+            byte[] messageBytes = message.getBytes(StandardCharsets.UTF_8);
+            nc.publish("inventory", messageBytes);
+
+            Thread.sleep(200);
+
+        } catch (InterruptedException | IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
